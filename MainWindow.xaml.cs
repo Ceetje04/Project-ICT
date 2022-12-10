1b123f80a52859764cd26754e5782a922018e4f0
Cédric Vindevogel <r0944241@student.vives.be>
2022-12-11 00:00:04 +0100

    Toevoegen string splitsen

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -39,18 +39,6 @@ namespace Project_ICT
             }
         }
 
-        private void _serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
-        {
-            // Lees alle tekst in, tot je een 'nieuwe lijn symbool' binnenkrijgt.
-            // New line = '\n' = ASCII-waarde 10 = ALT 10. 
-            string receivedText = _serialPort.ReadLine();
-
-            // Geef de ontvangen data door aan een method die op de UI thread loopt.
-            // Doe dat via een Action delegate... Delegates en Events zullen 
-            // in detail behandeld worden in het vak OOP.
-            Dispatcher.Invoke(new Action<string>(UpdateLabel), receivedText);
-        }
-
         private void cbxComPorts_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (_serialPort != null)
@@ -62,19 +50,120 @@ namespace Project_ICT
                 {
                     _serialPort.PortName = cbxComPorts.SelectedItem.ToString();
                     _serialPort.Open();
-                }
+                }   
             }
         }
 
-        private void UpdateLabel(string text)
+        private void _serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // Lees alle tekst in, tot je een 'nieuwe lijn symbool' binnenkrijgt.
+            // New line = '\n' = ASCII-waarde 10 = ALT 10. 
+            string receivedText = _serialPort.ReadLine();
+
+            //// Geef de ontvangen data door aan een method die op de UI thread loopt.
+            //// Doe dat via een Action delegate... Delegates en Events zullen 
+            //// in detail behandeld worden in het vak OOP.
+            Dispatcher.Invoke(new Action<string>(UpdateLabelData), receivedText);
+        }
+
+        public void split_string(string text)
+        {
+            string arduino = text;
+            string[] var = arduino.Split(new char[] {';'}, 3);
+
+            string temperatuur = var[0];
+            UpdateLabelTemp(temperatuur);
+
+            string vochtigheid = var[1];
+            UpdateLabelVocht(vochtigheid);
+
+            string luchtdruk = var[2];
+            UpdateLabelDruk(luchtdruk);
+
+        }
+
+        private void UpdateLabelData(string text)
         {
             lblData.Content = text;
         }
-        private void split_string()
-        { }
+
+
+        private void UpdateLabelTemp(double temperatuur) //Dit werkt niet omdat temperatuur een string is.
+        {
+            temperatuur = Convert.ToDouble(temperatuur);
+            lblTemp.Content = temperatuur;
+
+
+            if (temperatuur < 15)
+            {
+                lblDrukAlarm.Content = "opgepast voor lage temperatuur!";
+                lblDrukAlarm.Background = new SolidColorBrush(Colors.Red);
+            }
+            if (temperatuur > 25)
+            {
+                lblDrukAlarm.Content = "opgepast voor hoge temperatuur!";
+                lblDrukAlarm.Background = new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                lblDrukAlarm.Content = "Goede temperatuur!";
+                lblDrukAlarm.Background = new SolidColorBrush(Colors.Green);
+            }
+        }
+
+
+        private void UpdateLabelVocht(double vochtigheid)
+        {
+            vochtigheid = Convert.ToDouble(vochtigheid);
+            lblVocht.Content = vochtigheid;
+
+            if (vochtigheid < 30)
+            {
+                lblDrukAlarm.Content = "opgepast voor lage luchtvochtigeid!";
+                lblDrukAlarm.Background = new SolidColorBrush(Colors.Red);
+            }
+            if (vochtigheid > 70)
+            {
+                lblDrukAlarm.Content = "opgepast voor hoge luchtvochtigheid!";
+                lblDrukAlarm.Background = new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                lblDrukAlarm.Content = "Goede luchtvochtigheid!";
+                lblDrukAlarm.Background = new SolidColorBrush(Colors.Green);
+            }
+        }
+
+        private void UpdateLabelDruk(double luchtdruk)
+        {
+            luchtdruk = Convert.ToDouble(luchtdruk);
+            lblDruk.Content = luchtdruk;
+
+            if(luchtdruk < 950)
+            {
+                lblDrukAlarm.Content = "opgepast voor lage luchtdruk!";
+                lblDrukAlarm.Background = new SolidColorBrush(Colors.Red);
+            }
+            if(luchtdruk > 1060)
+            {
+                lblDrukAlarm.Content = "opgepast voor hoge luchtdruk!";
+                lblDrukAlarm.Background = new SolidColorBrush(Colors.Red);
+            }
+            else
+            {
+                lblDrukAlarm.Content = "Goede luchtdruk!";
+                lblDrukAlarm.Background = new SolidColorBrush(Colors.Green);
+            }
+        }
+
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-
+            
+        }
+        // Sluit de window wanneer op de afsluitknop gedrukt wordt.
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
